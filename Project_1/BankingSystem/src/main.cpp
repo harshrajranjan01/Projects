c508a9676e95e1f7d72c252eb8c9be9b4519a199
Harsh Raj Ranjan <harshrajranjan2005@gmail.com>
2026-01-05 01:43:07 +0530

    add create user in main menu

---
+++ Project_1/BankingSystem/src/main.cpp
@@ -0,0 +1,91 @@
+#include <iostream>
+#include "services/BankService.h"
+
+int main()
+{
+    BankService bank;
+    int choice = -1;
+    int currentUserId = -1; //-1 mean no user login
+
+    while (true)
+    {
+        // Not Logged in menu
+
+        if (currentUserId == -1)
+        {
+            std::cout << "\n....Main Menu...\n";
+            std::cout << "1. Create User\n";
+            std::cout << "2. Create Account\n";
+            std::cout << "3. Login\n";
+            std::cout << "0. Exit\n";
+            std::cout << "Enter Your choice\n";
+            std::cin >> choice;
+
+            if (choice == 1)
+            {   
+                std::string name;
+                int pin;
+
+                std::cout<<"Enter Name: \n";
+                std::cin>>name;
+
+                std::cout<<"Enter Pin: \n";
+                std::cin>>pin;
+
+                User user=bank.createUser(name,pin);
+
+                std::cout<<"User Created Sucessfully!\n";
+                std::cout<<"Your User Id is: "<<user.getUserId()<<"\n";
+            }
+           else if (choice == 2)
+            {
+            }
+           else if (choice == 3)
+            {
+            }
+            else if (choice == 0)
+            {
+                break;
+            }
+            else
+            {
+                std::cout << "Invalid choice\n";
+            }
+        }
+        // Logged In menu
+        else
+        {
+            std::cout << "\n.....User Menu....\n";
+            std::cout << "1. Deposite\n";
+            std::cout << "2. Withdraw\n";
+            std::cout << "3. Check Balance\n";
+            std::cout << "4. Logout\n";
+            std::cout << "Enter Your Choice\n";
+
+            std::cin >> choice;
+
+            if (choice == 1)
+            {
+            }
+
+            else if (choice == 2)
+            {
+            }
+
+            else if (choice == 3)
+            {
+            }
+
+            else if (choice == 4)
+            {
+                currentUserId == -1;
+            }
+            else
+            {
+                std::cout << "Invalid Choice\n";
+            }
+        }
+    }
+    std::cout << "Thank U" << std::endl;
+    return 0;
+}
